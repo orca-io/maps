820db6e5bf0f41a542c7cc860c88625258bd5a27
Nazar Shcherbiak <nazar.shcherbiak@getorca.com>
2026-07-22 20:59:23 +0300

    Provide focal point

--- ios/RNMBX/RNMBXMapView.swift
+++ ios/RNMBX/RNMBXMapView.swift
@@ -605,6 +605,7 @@ open class RNMBXMapView: UIView, RCTInvalidating {
     #if RNMBX_11
     var simultaneousRotateAndPinchZoomEnabled: Bool? = nil;
     #endif
+    var focalPoint: CGPoint? = nil;
   }
 
   var gestureSettings = GestureSettings()
@@ -654,6 +655,16 @@ open class RNMBXMapView: UIView, RCTInvalidating {
         options.simultaneousRotateAndPinchZoomEnabled = simultaneousRotateAndPinchZoomEnabled.boolValue
       }
 #endif
+      if let focalPoint = value["focalPoint"] as? NSDictionary {
+        if let x = focalPoint["x"] as? NSNumber, let y = focalPoint["y"] as? NSNumber {
+          options.focalPoint = CGPoint(x: x.doubleValue, y: y.doubleValue)
+        } else {
+          options.focalPoint = nil
+        }
+      } else {
+        options.focalPoint = nil
+      }
+
       /* android only
        if let zoomAnimationAmount = value["zoomAnimationAmount"] as? NSNumber {
        options.zoomAnimationAmount = zoomAnimationAmount.CGFloat
@@ -704,6 +715,7 @@ open class RNMBXMapView: UIView, RCTInvalidating {
         options.simultaneousRotateAndPinchZoomEnabled = simultaneousRotateAndPinchZoomEnabled
       }
 #endif
+      options.focalPoint = settings.focalPoint
       /* android only
        if let zoomAnimationAmount = value["zoomAnimationAmount"] as? NSNumber {
        options.zoomAnimationAmount = zoomAnimationAmount.CGFloat
